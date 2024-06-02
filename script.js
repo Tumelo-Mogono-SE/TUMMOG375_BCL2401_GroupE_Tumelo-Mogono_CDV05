fe24e84679f3d089d4ab3ea6421c38d5cb613480
Tumelo Mogono <tumelomogono@gmail.com>
2024-06-02 21:56:18 +0200

    Added script for toggling menu in smaller screen sizes

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const mobileNav = document.querySelector('.mobile-nav');
+
+    menuToggle.addEventListener('click', function() {
+        mobileNav.classList.toggle('active');
+    });
+});
